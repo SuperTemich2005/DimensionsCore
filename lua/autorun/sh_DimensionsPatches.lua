de94ec677ccbd3ee87fd874f05a9039517bcf6d2
SuperTemich2005 <supertemich2005@gmail.com>
2025-11-15 17:13:55 -0400

    Testing new trace lib

--- lua/autorun/sh_DimensionsPatches.lua
+++ lua/autorun/sh_DimensionsPatches.lua
@@ -50,3 +50,36 @@ hook.Add("PlayerSwitchWeapon","Modify Toolgun to Not Trace Extradimensional Enti
     end)
 end)
 
+local baseTraceLine = util.TraceLine
+util.TraceLine = function(traceData)
+    local baseFilter = traceData.filter
+    print("Modified TraceLine call!")
+    local candidates = ents.FindInBox(traceData.start-Vector(1,1,1),traceData.start+Vector(1,1,1))
+    print("Trying to figure out entity that sent the trace. Candidates: ")
+    PrintTable(candidates)
+    local originator = candidates[1]
+    for _, candidate in pairs(candidates) do
+        print("Dotting ",candidate,", dot product: ",candidate:GetForward():GetNormalized()," dot ",(traceData.end-traceData.start):GetNormalized()," = ",candidate:GetForward():GetNormalized():Dot((traceData.end-traceData.start):GetNormalized()))
+        if candidate:GetForward():GetNormalized():Dot((traceData.end-traceData.start):GetNormalized()) > originator:GetForward():GetNormalized():Dot((traceData.end-traceData.start):GetNormalized()) then
+            originator = candidate
+            print("New originator: ",originator)
+        end
+    end
+    traceData.filter = function(ent)
+        local returnValue = false
+        if IsValid(baseFilter) then
+            if type(baseFilter) == "table" then
+                returnValue = not table.HasValue(baseFilter,ent) -- Set returnValue to be true if ent is not in baseFilter (table)
+            elseif type(baseFilter) == "Entity" then
+                returnValue = baseFilter ~= ent -- Set returnValue to be true if ent is not baseFilter (entity)
+            end
+        end
+        if returnValue then -- If we detect that a trace might hit, we then check for dimensions
+            if originator:GetDimension() ~= ent:GetDimension() then
+                returnValue = false
+            end
+        end
+        return returnValue
+    end
+    baseTraceLine(traceData)
+end
