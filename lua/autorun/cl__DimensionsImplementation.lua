d9d97bc38458f113417ada7f00df8460ab4ce573
Nova <mariofluddd@gmail.com>
2025-11-15 17:56:28 -0400

    fixed hook

--- lua/autorun/cl__DimensionsImplementation.lua
+++ lua/autorun/cl__DimensionsImplementation.lua
@@ -1,4 +1,4 @@
-hook.Add("OnPlayerChat", "make players very lonely across dimensions", function( index, name, text, type )
+hook.Add("OnPlayerChat", "make players very lonely across dimensions", function(ply, text, temChat, isDead)
     if(ply:GetDimension() ~= LocalPlayer():GetDimension()) then return false end
 
     --probably add a way for players to talk cross dimension anyway, but dont make it default :3
