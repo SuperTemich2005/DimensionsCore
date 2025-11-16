b5c52d59549b0ba1e2616c7649cbe1ce2c1f931a
SuperTemich2005 <supertemich2005@gmail.com>
2025-11-16 16:02:10 -0400

    Applied traceline fix to tracehull

--- lua/autorun/sh_dimensionspatches.lua
+++ lua/autorun/sh_dimensionspatches.lua
@@ -128,3 +128,74 @@ util.TraceLine = function(traceData)
     traceData.filter = newFilter
     return baseTraceLine(traceData)
 end
+
+local baseTraceHull = util.TraceHull
+util.TraceHull = function(traceData)
+    local baseFilter = traceData.filter
+    local originator = nil
+    local traceDimension = DEFAULT_DIMENSION
+    
+    -- Figure out what dimension we're in
+    if baseFilter then
+        if type(baseFilter) == "Entity" or type(baseFilter) == "Player" then
+            originator = baseFilter -- Assume that the filtered entity is the originator, because why would a filter be just one entity if it's not the caller?
+            traceDimension = originator:GetDimension()
+        else
+            -- Now we need to use the expensive calculation
+            local startpos = traceData.start
+            local traceDir = (traceData.endpos-traceData.start):GetNormalized()
+            
+            local candidates = ents.FindInSphere(startpos,64)
+            if #candidates > 0 then
+                originator = candidates[1]
+                local originatorBestScore = originator:GetForward():Dot(traceDir)
+                for _, candidate in pairs(candidates) do
+                    local thisScore = candidate:GetForward():Dot(traceDir)
+                    if thisScore > originatorBestScore then
+                        originator = candidate
+                        originatorBestScore = thisScore
+                    end
+                    if originatorBestScore > 0.9999 then break end
+                end
+                traceDimension = originator:GetDimension()
+            end
+        end
+    end
+    
+    -- Build new filter
+    local newFilter = nil
+    if baseFilter then
+        if type(baseFilter) == "table" then
+            newFilter = table.Copy(baseFilter)
+            for dimensionName, dimensionTable in pairs(DimensionTables) do
+                if dimensionName ~= traceDimension then
+                    for v, _ in pairs(dimensionTable) do
+                        if not IsValid(v) then continue end
+                        newFilter[#newFilter+1] = v
+                    end
+                end
+            end
+        elseif type(baseFilter) == "Entity" or type(baseFilter) == "Player" then
+            newFilter = {baseFilter}
+            for dimensionName, dimensionTable in pairs(DimensionTables) do
+                if dimensionName ~= traceDimension then
+                    for v, _ in pairs(dimensionTable) do
+                        if not IsValid(v) then continue end
+                        newFilter[#newFilter+1] = v
+                    end
+                end
+            end
+        elseif type(baseFilter) == "function" then
+            newFilter = function(ent)
+                if ent:GetDimension() ~= traceDimension then return false end
+                return baseFilter(ent)
+            end
+        end
+    else
+        newFilter = function(ent) 
+            return ent:GetDimension() == traceDimension
+        end
+    end
+    traceData.filter = newFilter
+    return baseTraceHull(traceData)
+end
